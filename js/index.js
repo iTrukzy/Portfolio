3495bba8a99f783e554816d2cc6ff67396591ea9
iTrukzy <74843848+iTrukzy@users.noreply.github.com>
2021-04-10 13:35:52 -0500

    contact

--- js/index.js
+++ js/index.js
@@ -7,8 +7,6 @@ document.getElementById('navbar__close').addEventListener('click', () => {
     navbarMovil.classList.remove('show')
 })
 
-
-
 window.addEventListener('scroll', () => {
     let scrollUp = document.getElementById('scroll__up')
     let navbar = document.getElementById('navbar__site')
@@ -16,4 +14,41 @@ window.addEventListener('scroll', () => {
     scrollUp.classList.toggle('showUp', window.scrollY > 0)
     navbar.classList.toggle('sticky', window.scrollY > 0)
     scrollDown.classList.toggle('scroll__rotate', window.scrollY > 0)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const getProjects = () => {
+        fetch('./proyects.json')
+            .then(res => res.json())
+            .then(data => printProjects(data))
+    }
+
+    const printProjects = (data) => {
+        console.log(data)
+        const projectsContainer = document.querySelector('.projects__body')
+    
+        const row = document.createElement('div')
+        row.classList.add('row')
+
+        let html = ``;
+        data.forEach(items => {
+            html += `<div data-aos="fade-up" class="items__json col s12 m4 l4">
+            <img src=${items.img} alt="${items.title}">
+                <div class="hover__items">
+                <h6>${items.title}</h6>
+                <p>${items.description}</p>
+                <a href="${items.link}" target="_blank">Link</a>
+                </div>
+            </div>`
+        });
+
+
+
+        row.innerHTML = html
+        projectsContainer.appendChild(row)
+    }
+
+
+
+    getProjects()
 })
